16853c6a9d9c768869afa32c90b16356578452b3
threeup <threeup@gmail.com>
2019-10-12 23:47:10 -0700

    no redir

--- Startup.cs
+++ Startup.cs
@@ -42,7 +42,7 @@ namespace homeangular
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             if (!env.IsDevelopment())
             {
